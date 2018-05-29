ae59ad187685110011872046f011d4580d07e619
Derick Castillo & Alan Uraz <appacademy@Apps-Mac-mini-2.local>
2018-05-29 11:54:40 -0400

    Initialize board and cursor

--- Chess/display.rb
+++ Chess/display.rb
@@ -1,1 +1,12 @@
 require 'colorize'
+require 'cursor.rb'
+require 'board.rb'
+
+class Display
+  
+  def initialize(board)
+    @cursor = Cursor.new([0,0], board)
+    @board = board
+  end 
+  
+end 
